fb4425594e2d221baaffa13fbb09a7106a70f56d
Liam Williams <l.s.w5559@gmail.com>
2017-12-05 00:51:35 +0000

    Further SVG support

---
+++ XF/FileWrapper.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liam
+ * Date: 23/11/2017
+ * Time: 12:32 AM
+ */
+
+namespace SV\AttachmentImprovements\XF;
+
+use SV\AttachmentImprovements\SvgImage;
+
+class FileWrapper extends XFCP_FileWrapper
+{
+	protected $isSvg;
+	/** @var SvgImage */
+	protected $svgImage = null;
+
+	public function getImageType()
+	{
+		if ($this->isSvg)
+		{
+			return SvgImage::IMAGETYPE_SVG;
+		}
+
+		return parent::getImageType();
+	}
+
+	public function getImageWidth()
+	{
+		if ($this->isImage() && $this->isSvg)
+		{
+			return $this->svgImage->getDimensions()['width'];
+		}
+
+		return parent::getImageWidth();
+	}
+
+	public function getImageHeight()
+	{
+		if ($this->isImage() && $this->isSvg)
+		{
+			return $this->svgImage->getDimensions()['height'];
+		}
+
+		return parent::getImageHeight();
+	}
+
+	protected function analyzeImage()
+	{
+		$this->isImage = false;
+
+		if (!$this->fileSize)
+		{
+			return;
+		}
+
+		if ($this->extension == 'svg')
+		{
+			$this->svgImage = new SvgImage($this->filePath);
+			$this->isSvg = true;
+		}
+
+		if (!$this->isSvg)
+		{
+			$this->svgImage = null;
+			parent::analyzeImage();
+		}
+		else
+		{
+			$this->isImage = true;
+		}
+	}
+}
--- _output/extension_hint.php
+++ _output/extension_hint.php
@@ -3,6 +3,16 @@
 // ################## THIS IS A GENERATED FILE ##################
 // DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
 
+namespace SV\AttachmentImprovements\XF
+{
+	class XFCP_FileWrapper extends \XF\FileWrapper {}
+}
+
+namespace SV\AttachmentImprovements\XF\Http
+{
+	class XFCP_Upload extends \XF\Http\Upload {}
+}
+
 namespace SV\AttachmentImprovements\XF\Pub\View\Attachment
 {
 	class XFCP_View extends \XF\Pub\View\Attachment\View {}
