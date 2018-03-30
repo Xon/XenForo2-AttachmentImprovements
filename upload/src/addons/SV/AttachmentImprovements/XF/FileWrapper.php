e444211294ad660d801858d41550e8f7b3e12c9d
Xon <tazipper1@hotmail.com>
2018-03-30 17:41:29 +0800

    tabs => spaces

--- upload/src/addons/SV/AttachmentImprovements/XF/FileWrapper.php
+++ upload/src/addons/SV/AttachmentImprovements/XF/FileWrapper.php
@@ -12,63 +12,63 @@ use SV\AttachmentImprovements\SvgImage;
 
 class FileWrapper extends XFCP_FileWrapper
 {
-	protected $isSvg;
-	/** @var SvgImage */
-	protected $svgImage = null;
+    protected $isSvg;
+    /** @var SvgImage */
+    protected $svgImage = null;
 
-	public function getImageType()
-	{
-		if ($this->isSvg)
-		{
-			return SvgImage::IMAGETYPE_SVG;
-		}
+    public function getImageType()
+    {
+        if ($this->isSvg)
+        {
+            return SvgImage::IMAGETYPE_SVG;
+        }
 
-		return parent::getImageType();
-	}
+        return parent::getImageType();
+    }
 
-	public function getImageWidth()
-	{
-		if ($this->isImage() && $this->isSvg)
-		{
-			return $this->svgImage->getDimensions()['width'];
-		}
+    public function getImageWidth()
+    {
+        if ($this->isImage() && $this->isSvg)
+        {
+            return $this->svgImage->getDimensions()['width'];
+        }
 
-		return parent::getImageWidth();
-	}
+        return parent::getImageWidth();
+    }
 
-	public function getImageHeight()
-	{
-		if ($this->isImage() && $this->isSvg)
-		{
-			return $this->svgImage->getDimensions()['height'];
-		}
+    public function getImageHeight()
+    {
+        if ($this->isImage() && $this->isSvg)
+        {
+            return $this->svgImage->getDimensions()['height'];
+        }
 
-		return parent::getImageHeight();
-	}
+        return parent::getImageHeight();
+    }
 
-	protected function analyzeImage()
-	{
-		$this->isImage = false;
+    protected function analyzeImage()
+    {
+        $this->isImage = false;
 
-		if (!$this->fileSize)
-		{
-			return;
-		}
+        if (!$this->fileSize)
+        {
+            return;
+        }
 
-		if ($this->extension == 'svg')
-		{
-			$this->svgImage = new SvgImage($this->filePath);
-			$this->isSvg = true;
-		}
+        if ($this->extension == 'svg')
+        {
+            $this->svgImage = new SvgImage($this->filePath);
+            $this->isSvg = true;
+        }
 
-		if (!$this->isSvg)
-		{
-			$this->svgImage = null;
-			parent::analyzeImage();
-		}
-		else
-		{
-			$this->isImage = true;
-		}
-	}
+        if (!$this->isSvg)
+        {
+            $this->svgImage = null;
+            parent::analyzeImage();
+        }
+        else
+        {
+            $this->isImage = true;
+        }
+    }
 }
