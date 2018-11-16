37577fb9f54c369d66b0111ae23d5a8c22d141f4
Xon <tazipper1@hotmail.com>
2018-11-16 09:17:25 +0800

    Add XAR support to admincp

--- upload/src/addons/SV/AttachmentImprovements/XF/Admin/View/Attachment/View.php
+++ upload/src/addons/SV/AttachmentImprovements/XF/Admin/View/Attachment/View.php
@@ -2,12 +2,45 @@
 
 namespace SV\AttachmentImprovements\XF\Admin\View\Attachment;
 
-
+use SV\AttachmentImprovements\InternalPathUrlSupport;
+use SV\AttachmentImprovements\SvgResponse;
 
 /**
  * Extends \XF\Admin\View\Attachment\View
  */
 class View extends XFCP_View
 {
+    public function renderRaw()
+    {
+        if (!empty($this->params['return304']))
+        {
+            return parent::renderRaw();
+        }
 
+        SvgResponse::updateInlineImageTypes($this->response, 'svg', 'image/svg+xml');
+
+        $options = \XF::options();
+        if ($options->SV_AttachImpro_XAR)
+        {
+            /** @var \XF\Entity\Attachment $attachment */
+            $attachment = $this->params['attachment'];
+
+            $attachmentFile = $attachment->Data->getAbstractedDataPath();
+            if ($attachmentFile = InternalPathUrlSupport::convertAbstractFilenameToURL($attachmentFile))
+            {
+                if (\XF::$debugMode && $options->SV_AttachImpro_log)
+                {
+                    \XF::logError('X-Accel-Redirect:' . $attachmentFile, true);
+                }
+                $this->response
+                    ->setAttachmentFileParams($attachment->filename, $attachment->extension)
+                    ->header('ETag', '"' . $attachment->attach_date . '"')
+                    ->header('X-Accel-Redirect', $attachmentFile);
+
+                return '';
+            }
+        }
+
+        return parent::renderRaw();
+    }
 }
