3fbbc6794308a37d47da568d997da7e90adb12f9
Xon <635541+Xon@users.noreply.github.com>
2022-12-13 12:29:44 +0800

    Use SV/StandardLib installer bits

--- upload/src/addons/SV/AttachmentImprovements/Setup.php
+++ upload/src/addons/SV/AttachmentImprovements/Setup.php
@@ -2,6 +2,7 @@
 
 namespace SV\AttachmentImprovements;
 
+use SV\StandardLib\InstallerHelper;
 use XF\AddOn\AbstractSetup;
 use XF\AddOn\StepRunnerInstallTrait;
 use XF\AddOn\StepRunnerUninstallTrait;
@@ -9,6 +10,7 @@ use XF\AddOn\StepRunnerUpgradeTrait;
 
 class Setup extends AbstractSetup
 {
+    use InstallerHelper;
     use StepRunnerInstallTrait;
     use StepRunnerUpgradeTrait;
     use StepRunnerUninstallTrait;
