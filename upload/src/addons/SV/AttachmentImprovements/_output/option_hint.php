1c5c8d0959030c1eb8948fe94b865c33008b4f40
Xon <635541+Xon@users.noreply.github.com>
2025-02-23 06:45:02 +0800

    XF2.3 export

---
+++ upload/src/addons/SV/AttachmentImprovements/_output/option_hint.php
@@ -0,0 +1,26 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property bool|null $SV_AttachImpro_XAR Nginx X-Accel-Redirect Support
+ * @property string|null $SV_AttachImpro_badAttributes SVG Bad Attributes
+ * @property string|null $SV_AttachImpro_badTags Bad SVG tags
+ * @property bool|null $SV_AttachImpro_log Log Attachment Redirection URLs
+ * @property bool|null $SV_RejectAttachmentWithBadTags Reject SVG attachment with bad svg tags/attributes
+ * @property bool|null $svAllowInlineDisplayOfSvgAttachments Allow inline display of SVG image attachments
+ * @property bool|null $svAttachmentImprov_svgAdminOnly Only admins can upload SVGs as images
+ * @property bool|null $svAttachmentsStripExif Strip JPEG EXIF
+ * @property int|null $svPartialContentChunkSize Partial content maximum chunk size
+ */
+class Options
+{
+}
